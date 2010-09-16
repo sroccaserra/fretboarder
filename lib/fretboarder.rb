bbdcde3c77d34f33577606acb1203e9656e1d211
sroccaserra <sroccaserra@yahoo.com>
2010-09-16 13:36:45 +0200

    settings now use an openstruct

--- lib/fretboarder.rb
+++ lib/fretboarder.rb
@@ -5,6 +5,7 @@
 #
 
 require 'optparse'
+require 'ostruct'
 require 'ncurses'
 
 $NOTES = [["e", "f", "^f", "g", "^g", "a", "^a", "b", "c'", "^c'", "d'", "^d'", "e'"],
@@ -24,14 +25,6 @@ $FLAT_NOTES = [["e", "f", "_g", "g", "_a", "a", "_b", "b", "c'", "_d'", "d'", "_
 $NB_STRINGS = $NOTES.size
 $NB_FRETS = $NOTES[0].size - 1
 
-$settings = {
-    :period => 3,
-    :start => 0,
-    :end => $NB_FRETS,
-    :use_flats => false,
-    :display_map => false
-}
-
 class FretQuestion
     attr_reader :stringNumber, :fretNumber
     def initialize stringNumber, fretNumber
@@ -41,8 +34,8 @@ class FretQuestion
 end
 
 class KeyboardAnswer
-    def initialize char = nil
-        @noteName = note_for_character char
+    def initialize char
+        @noteName = char ? note_for_character(char) : :invalid
     end
 
     def noteName
@@ -54,11 +47,11 @@ class KeyboardAnswer
     end
 
     def isValid?
-        return nil != @noteName && !mustQuit?
+        return :invalid != @noteName
     end
 
     def note_for_character character
-        use_sharps = !$settings[:use_flats]
+        use_sharps = !$settings.use_flats
         b2 = use_sharps ? '^c' : '_d'
         table = {
             ?a => 'c',
@@ -79,6 +72,7 @@ class KeyboardAnswer
             ?\e => :quit,
             ?\C-c => :quit
         }
+        table.default = :invalid
         table[character]
     end
 
@@ -102,7 +96,7 @@ class Fretboard
     end
 
     def answerTo question
-        notes = $settings[:use_flats] ? $FLAT_NOTES : $NOTES
+        notes = $settings.use_flats ? $FLAT_NOTES : $NOTES
         notes[question.stringNumber-1][question.fretNumber]
     end
 
@@ -185,8 +179,8 @@ def random_string
     rand($NB_STRINGS) + 1
 end
 
-def random_fret
-    rand($settings[:end] - $settings[:start] + 1) + $settings[:start]
+def random_fret min, max
+    rand(max - min + 1) + min
 end
 
 $COLOR = {
@@ -197,7 +191,7 @@ $COLOR = {
     :red => 4,
 }
 
-def quizz
+def quizz settings
     stats = {
         :start => Time.new,
         :success => 0,
@@ -207,14 +201,14 @@ def quizz
     }
 
     question = nil
-    answer = KeyboardAnswer.new
+    answer = KeyboardAnswer.new :none
     window = Ncurses.stdscr
     while :quit != answer.noteName do
         window.clear
         window.mvaddstr 1, 0, "Question #{stats[:nbQuestions]}:"
         fretboard = Fretboard.new
         old_question = question
-        question = FretQuestion.new random_string, random_fret
+        question = FretQuestion.new random_string, random_fret(settings.start, settings.end)
         fretboard.ask! question
         if old_question
             result = fretboard.gradeAnswer! answer, old_question
@@ -228,11 +222,17 @@ def quizz
         stats[:nbQuestions] += 1
         start = Time.new
         window.mvaddstr 11, 0, "#{stats[:success]} success, #{stats[:slow]} slow, #{stats[:failure]} errors."
-        window.mvaddstr 13, 0, "A, S, D, F,... to answer (corresponds to C, D, E, F,...), and ESC to quit."
+        directions = """Press:
+- ASDFGHJ keys to answer natural notes (C, D, E, F, G, A, B)
+- WE TYU for altered notes
+- ESC or C-c to quit.
+
+Note: QSDFGHJ and WE TYU for AZERTY users."""
+        window.mvaddstr 13, 0, directions
         window.refresh
 
         answerChar = Ncurses.getch
-        isAnswerSlow = Time.new - start > $settings[:period]
+        isAnswerSlow = Time.new - start > settings.period
         if isAnswerSlow
             answer = SlowKeyboardAnswer.new answerChar
             stats[:slow] += 1
@@ -243,7 +243,7 @@ def quizz
     stats
 end
 
-def auto_quizz
+def auto_quizz settings
     nbQuestions = 1
     question = nil
 
@@ -251,18 +251,18 @@ def auto_quizz
     Ncurses.stdscr.nodelay true
     Ncurses.timeout 0
     key = nil
-    start = Time.new - $settings[:period]
+    start = Time.new - settings.period
 
     while ?\e != key && ?\C-c != key do
         key = Ncurses.getch
-        if Time.new - start >= $settings[:period]
+        if Time.new - start >= settings.period
             fretboard = Fretboard.new
             previous_question = question
             if previous_question then
                 fretboard.giveAnswerTo! previous_question
             end
 
-            question = FretQuestion.new random_string, random_fret
+            question = FretQuestion.new random_string, random_fret(settings.start, settings.end)
             fretboard.ask! question
 
             window.clear
@@ -298,30 +298,41 @@ def display_map
     Ncurses.getch
 end
 
-if __FILE__ == $0
+def default_settings
+    {
+        :period => 3,
+        :start => 0,
+        :end => $NB_FRETS,
+        :use_flats => false,
+        :display_map => false
+    }
+end
 
+$settings = OpenStruct.new default_settings
+
+if __FILE__ == $0
     option_parser = OptionParser.new do |opts|
         opts.banner = """Usage: #{__FILE__} [options]
 Note: see http://abcnotation.com about note notations.
 
 """
-        opts.on("-p", "--period [TIME]", "Period in seconds between each question (default: #{$settings[:period]})") do |p|
-            $settings[:period] = Float(p)
+        opts.on("-p", "--period [TIME]", "Period in seconds between each question (default: #{$settings.period})") do |p|
+            $settings.period = Float(p)
         end
-        opts.on("-s", "--start [FRET]", "Starting fret for questions (default: #{$settings[:start]})") do |s|
-            $settings[:start] = Integer(s)
+        opts.on("-s", "--start [FRET]", "Starting fret for questions (default: #{$settings.start})") do |s|
+            $settings.start = Integer(s)
         end
-        opts.on("-e", "--end [FRET]", "Ending fret for questions (default: #{$settings[:end]})") do |e|
-            $settings[:end] = Integer(e)
+        opts.on("-e", "--end [FRET]", "Ending fret for questions (default: #{$settings.end})") do |e|
+            $settings.end = Integer(e)
         end
         opts.on("-b", "--use-flats", "Use flats (default: use sharps)") do |b|
-            $settings[:use_flats] = b
+            $settings.use_flats = b
         end
         opts.on("-m", "--display-map", "Shows a fretboard map and exits") do |m|
-            $settings[:display_map] = m
+            $settings.display_map = m
         end
         opts.on("-a", "--auto", "In this mode inputs are ignored: the answer periodically shows up with another question.") do |a|
-            $settings[:auto] = a
+            $settings.auto = a
         end
 
         opts.on_tail("-h", "--help", "Show this message and exits") do
@@ -359,12 +370,12 @@ Note: see http://abcnotation.com about note notations.
         #        Ncurses.timeout 0
         Ncurses.raw
         #        Ncurses.stdscr.keypad true
-        if $settings[:display_map]
+        if $settings.display_map
             display_map
-        elsif $settings[:auto]
-            auto_quizz
+        elsif $settings.auto
+            auto_quizz $settings
         else
-            quizz
+            quizz $settings
         end
     ensure
         Ncurses.curs_set 1
